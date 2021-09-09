b425ce27c128798e9b8a7894d5acc1ff180d8428
p_ggrise <geraldo.grise@rodobens.com.br>
2021-09-09 09:05:04 -0300

    github

---
+++ CalculoJuros/CalculoJuros/Controllers/GithubController.cs
@@ -0,0 +1,27 @@
+﻿using Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CalculoJuros.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class GithubController : ControllerBase
+    {
+        private static string GITHUBREPOSITORY = "https://github.com/geraldogrise/softplan";
+        public GithubController()
+        {
+            
+        }
+
+        [HttpGet("showmethecode")]
+        public ActionResult ShowMeTheCode()
+        {
+            return Ok(GITHUBREPOSITORY);
+        }
+    }
+}
